68350839763aa4ec44712e795ff329b81f4b6374
Claudio Paladini <clode.paladini@gmail.com>
2022-11-09 20:49:53 +0000

    data documentatio
    Created data.R

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' example dataset
+#'
+#' @format A tibble representing sales and other variables over time.
+"sales_ts"
+
+#' example pooled dataset
+#'
+#' @format A tibble representing google trends variables over time.
+"pooled_gt_data"
